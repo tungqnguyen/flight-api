a01bf1730093ea8b1c1a1de43254b57022765db3
qtngu8 <qtngu8@student.monash.edu>
2019-06-14 16:46:15 +1000

    update db-init

--- db/databases/db-init.js
+++ db/databases/db-init.js
@@ -22,60 +22,65 @@ function createAirportDbCol(array) {
   return result;
 }
 async function airportDbInit() {
-  try {
-    db.run('DROP TABLE IF EXISTS airport');
-    console.log('creating airport table...');
-    const response = await axios.get('http://ourairports.com/data/airports.csv');
-    parse(response.data, (err, records) => {
-      const cols = createAirportDbCol(records[0]);
-      const sql = `CREATE TABLE IF NOT EXISTS airport ( ${cols} )`;
-      const placeholders = records[0].map(value => '?').join(',');
+  return new Promise(async (resolve, reject) => {
+    try {
+      db.run('DROP TABLE IF EXISTS airport');
+      console.log('creating airport table...');
+      const response = await axios.get('http://ourairports.com/data/airports.csv');
+      parse(response.data, (err, records) => {
+        const cols = createAirportDbCol(records[0]);
+        const sql = `CREATE TABLE IF NOT EXISTS airport ( ${cols} )`;
+        const placeholders = records[0].map(value => '?').join(',');
+        db.serialize(() => {
+          db.run(sql);
+          db.run('BEGIN');
+          console.log('inserting airport records...');
+          const stmt = db.prepare(`INSERT INTO airport VALUES (${placeholders})`);
+          for (let i = 1; i < records.length; i += 1) {
+            stmt.run(records[i]);
+          }
+          stmt.finalize();
+          db.run('COMMIT');
+          console.log('airport table created');
+          resolve();
+        });
+      });
+    } catch (error) {
+      console.log('error while downloading airports data', error);
+    }
+  });
+}
+async function flightDbInit() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const KEY = '84d2f1-bd395b';
+      db.run('DROP TABLE IF EXISTS flight');
+      console.log('creating flight table...');
+      const response = await axios.get(`http://aviation-edge.com/v2/public/flights?key=${KEY}`);
+      // eslint-disable-next-line max-len
+      const str = 'flight_iata CHAR, flight_icao CHAR, departure_iata CHAR, arrival_iata CHAR, departure_icao CHAR, arrival_icao CHAR, airline_iata CHAR';
+      const sql = `CREATE TABLE IF NOT EXISTS flight (${str})`;
+      const placeholders = '?, ?, ?, ?, ?, ?, ?';
       db.serialize(() => {
         db.run(sql);
         db.run('BEGIN');
-        console.log('inserting airport records...');
-        const stmt = db.prepare(`INSERT INTO airport VALUES (${placeholders})`);
-        for (let i = 1; i < records.length; i += 1) {
-          stmt.run(records[i]);
-        }
+        console.log('inserting flight records...');
+        const stmt = db.prepare(`INSERT INTO flight VALUES (${placeholders})`);
+        response.data.map((val) => {
+          const {
+            flight, departure, arrival, airline,
+          } = val;
+          stmt.run(flight.iataNumber, flight.icaoNumber, departure.iataCode, arrival.iataCode,
+            departure.icaoCode, arrival.icaoCode, airline.iataCode);
+        });
         stmt.finalize();
         db.run('COMMIT');
-        console.log('airport table created');
+        console.log('flight table created');
       });
-    });
-  } catch (error) {
-    console.log('error while downloading airports data', error);
-  }
-}
-async function flightDbInit() {
-  try {
-    const KEY = '84d2f1-bd395b';
-    db.run('DROP TABLE IF EXISTS flight');
-    console.log('creating flight table...');
-    const response = await axios.get(`http://aviation-edge.com/v2/public/flights?key=${KEY}`);
-    // eslint-disable-next-line max-len
-    const str = 'flight_iata CHAR, flight_icao CHAR, departure_iata CHAR, arrival_iata CHAR, departure_icao CHAR, arrival_icao CHAR, airline_iata CHAR';
-    const sql = `CREATE TABLE IF NOT EXISTS flight (${str})`;
-    const placeholders = '?, ?, ?, ?, ?, ?, ?';
-    db.serialize(() => {
-      db.run(sql);
-      db.run('BEGIN');
-      console.log('inserting flight records...');
-      const stmt = db.prepare(`INSERT INTO flight VALUES (${placeholders})`);
-      response.data.map((val) => {
-        const {
-          flight, departure, arrival, airline,
-        } = val;
-        stmt.run(flight.iataNumber, flight.icaoNumber, departure.iataCode, arrival.iataCode,
-          departure.icaoCode, arrival.icaoCode, airline.iataCode);
-      });
-      stmt.finalize();
-      db.run('COMMIT');
-      console.log('flight table created');
-    });
-  } catch (error) {
-    console.log('error while downloading flights data', error);
-  }
+    } catch (error) {
+      console.log('error while downloading flights data', error);
+    }
+  });
 }
 async function dbInit() {
   try {
